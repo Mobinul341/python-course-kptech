ab37d744278f10738ef32cfcbfc9ca2ad3d94ea2
Mobinul Kawshik <engr.kawshik@ymail.com>
2020-04-12 22:08:13 +0600

    Update class-10-11-dection.py
    I have updated it from my smartphone, that's indentation may not work properly. Please fix the indentation on your IDE to get the output.

--- Class-and-Practice/class-10-11-dection.py
+++ Class-and-Practice/class-10-11-dection.py
@@ -29,3 +29,9 @@ for key in users.keys():
     # print(values)
     # for mykeys, myvalues in values.item():
     #     print(mykeys)
+
+#Updated Loop and solution by Kawshik
+for keys,values in users.items():
+	print(keys.upper()) #upper() for capitalize the key only
+		for value, data in values.items():
+			print(value, ':', data) #nested loop to access nested dictionary
